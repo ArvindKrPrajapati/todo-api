bb25d84c35072bae32230f80c945047ca176ce8c
ArvindKrPrajapati <arvindsoft37@gmail.com>
2023-05-20 03:21:58 +0530

    unique movie

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -30,13 +30,58 @@ const getAll = async (req, res) => {
       filter["country"] = country;
     }
 
-    const data = await movie
+   /* const data = await movie
       .find(filter)
       .sort({
         release_date: -1,
       })
       .skip(skip)
-      .limit(limit);
+      .limit(limit);*/
+      
+      
+ 
+
+    const pipeline = [
+  {
+    $match: filter
+  },
+  {
+    $group: {
+      _id: '$tmdb_id',
+      count: { $sum: 1 },
+      document: { $first: '$$ROOT' }
+    }
+  },
+  {
+    $match: {
+      count: { $eq: 1 }
+    }
+  },
+  {
+    $sort: {
+      'document.release_date': -1
+    }
+  },
+  {
+    $skip: skip
+  },
+  {
+    $limit: limit
+  },
+  {
+    $replaceRoot: {
+      newRoot: '$document'
+    }
+  },
+  {
+    $project: {
+      _id: 0
+    }
+  }
+];
+
+
+    const data = await movie.aggregate(pipeline)
     return res.status(200).json({
       success: true,
       data,
