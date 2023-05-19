d78ce21a6210dbf1e456db7a7dd8143e75671e8a
ArvindKrPrajapati <arvindsoft37@gmail.com>
2023-05-20 02:24:55 +0530

    added aspect ratio btn

--- public/play/script.js
+++ public/play/script.js
@@ -1,4 +1,3 @@
-
 const params = new URLSearchParams(window.location.href.split("?")[1]);
 const url = "https://mytodo-api.cyclic.app/v1/mverse";
 const main = document.querySelector("#main");
@@ -10,48 +9,69 @@ if (!id) {
   alert("not found");
 } else {
   fetch(url + "/details/" + id)
-    .then((res) => res.json())
-    .then((data) => {
-      main.style.display = "block";
-      loader.style.display = "none";
-      if (data.success) {
-        if (data.data.length) {
-          const details = data.data[0];
-          let sources = data.data.map((item) => {
-            return {
-              file: item.video[0].href,
-              label: item.video[0].resolution + "p - " + item.video[0].language,
-              type: "video/mp4",
-            };
-          });
-          const h = `<section class="content">
-          <div class="layout">
-                <div id="player"></div>
-                 <div class="p-4">
-                   <h2 class="text-white mb-0">${details.title}</h2>
-                   <small style="color:gainsboro; display:block" class="mb-1">Release Date : ${details.release_date}</small><br>
-                    <small style="color:#eee">${details.overview}</small>
-                  </div>
+  .then((res) => res.json())
+  .then((data) => {
+    main.style.display = "block";
+    loader.style.display = "none";
+    if (data.success) {
+      if (data.data.length) {
+        const details = data.data[0];
+        let sources = data.data.map((item) => {
+          return {
+            file: item.video[0].href,
+            label: item.video[0].resolution + "p - " + item.video[0].language,
+            type: "video/mp4",
+          };
+        });
+        const h = `<section class="content">
+        <div class="layout">
+        <div id="player"></div>
+        <div class="p-4">
+        <h2 class="text-white mb-0">${details.title}</h2>
+        <small style="color:gainsboro; display:block" class="mb-1">Release Date : ${details.release_date}</small><br>
+        <small style="color:#eee">${details.overview}</small>
         </div>
         </div>
-          `;
+        </div>
+        `;
 
-          document.querySelector("#main").innerHTML = h;
-          var player = jwplayer("player");
-          player.setup({
-            // file: "http://hd1.dlmania.com/Hollywood/M3gan%20-%20SCam/M3gan%20-%20SCam%20HD%20(HDMp4Mania).mp4"
-            sources: sources,
-            autostart:true
-            // other configuration options
-          });
-        } else {
-          console.log("movie not found");
-        }
+        document.querySelector("#main").innerHTML = h;
+        var player = jwplayer("player");
+        player.setup({
+         // file: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+           sources: sources,
+          stretching: "uniform",
+        });
+        player.addButton(
+          "./aspect-ratio.svg",
+          "Toggle Resize",
+          function() {
+            // Click handler function
+            if (player.getStretching() === "uniform") {
+              player.setConfig({
+                stretching:"exactfit"
+              })
+
+            } else if(player.getStretching()==="exactfit") {
+              player.setConfig({
+                stretching: "fill"
+              });
+            }else{
+              player.setConfig({
+                stretching: "uniform"
+              });
+            }
+          },
+          "myButtonId"
+        );
       } else {
-        console.log("error");
+        console.log("movie not found");
       }
-    })
-    .catch((e) => {
-      console.log(e);
-    });
+    } else {
+      console.log("error");
+    }
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 }
